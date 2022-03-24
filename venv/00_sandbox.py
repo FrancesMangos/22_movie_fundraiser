c78015b227aa9097ee7a26b3e591e8a047cc1fc3
magno.franc <magno.franc@awatapu.school.nz>
2022-03-25 12:27:42 +1300

    got bored. might delete later

--- venv/00_sandbox.py
+++ venv/00_sandbox.py
@@ -1,2 +1,14 @@
-for i in range(1):
-    print("Yes!")
+import random
+
+name = input("What is your name?").title()
+age = int(input("How old are you?"))
+
+print("So you're {} and are {} years old?".format(name, age))
+
+money = int(input("How much money do you have?"))
+print("I'm gonna yoink 30% of your money from you!")
+
+taken = (money / 10) * 3
+money = money - taken
+
+print("I took {:.2f} so now you have: {:.2f}".format(taken, money))
