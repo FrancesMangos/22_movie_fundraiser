6c613ef25d8f6163b24c3c16b0b491a4875efbf6
magno.franc <magno.franc@awatapu.school.nz>
2022-03-28 10:17:43 +1300

    01 not blank done

---
+++ venv/01_not_blank_v1.py
@@ -0,0 +1,14 @@
+#functions go here
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("Sorry, this cannot be blank")
+
+#main program goes here
+name = not_blank("Name:")
