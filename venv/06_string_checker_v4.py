e766c911fa3dc9c6f86529f9b5cafaf1ef2330f0
magno.franc <magno.franc@awatapu.school.nz>
2022-04-14 14:40:45 +1200

    same thing for now

---
+++ venv/06_string_checker_v4.py
@@ -0,0 +1,31 @@
+def string_check(choice, options):
+
+    for var_list in options:
+
+        if choice in var_list:
+
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+
+        else:
+            is_valid == "no"
+
+    if is_valid == "yes":
+        return chosen
+    else:
+        return "invalid choice"
+
+valid_snacks = [
+    ["popcorn", "p", "corn", "a"],
+    ["M&M's", "m&m's", "mms", "m", "b"],
+    ["pita chips", "chips", "pc", "pita", "c"],
+    ["water", "w", "d"]
+]
+
+for item in range(0, 6):
+
+    desired_snack = input("Snack: ").lower()
+
+    snack_choice = string_check(desired_snack, valid_snacks)
+    print("Snack Choice: ", snack_choice)
