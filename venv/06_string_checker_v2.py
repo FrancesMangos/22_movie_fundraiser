d7f08bb4199c9c611a396314bde814b55a29dc41
magno.franc <magno.franc@awatapu.school.nz>
2022-04-14 14:34:19 +1200

    commit arson, not really tho

---
+++ venv/06_string_checker_v2.py
@@ -0,0 +1,37 @@
+
+# valid snacks holds list of all snacks
+# each item in valid snacks is a list with,
+# valid options for each snack - full name, letter code,
+# and any possible abbreviations
+valid_snacks = [
+    ["popcorn", "p", "corn", "a"],
+    ["M&M's", "m&m's", "mms", "m", "b"],
+    ["pita chips", "chips", "pc", "pita", "c"],
+    ["water", "w", "d"]
+]
+
+# variables
+snack_ok = ""
+snack = ""
+
+# loop three times to make testing quicker
+for item in range(0, 3):
+    desired_snack = input("Snack: ").lower()
+
+    for var_list in valid_snacks:
+
+        if desired_snack in var_list:
+
+            snack = var_list[0].title()
+            snack_ok = "yes"
+            break
+
+        else:
+            snack_ok = "no"
+
+    if snack_ok == "yes":
+        print("Snack Choice: ", snack)
+
+    else:
+        print("Invalid Choice")
+
