2fd57823a080fa0d174fed7e26425455178b6451
magno.franc <magno.franc@awatapu.school.nz>
2022-05-20 12:27:55 +1200

    cry

---
+++ venv/09_snack_analyser_v2.py
@@ -0,0 +1,86 @@
+import re
+
+
+def string_check(choice, options):
+    is_valid = ""
+    for var_list in options:
+
+        if choice in var_list:
+
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+
+        else:
+            is_valid == "no"
+
+    if is_valid == "yes":
+        return chosen
+    else:
+        return "invalid choice"
+
+
+number_regex = "^[1-9]"
+
+valid_snacks = [
+    ["popcorn", "p", "corn", "a"],
+    ["M&M's", "m&m's", "mms", "m", "b"],
+    ["pita chips", "chips", "pc", "pita", "c"],
+    ["water", "w", "d"]
+]
+
+# valid options for yes/no questions
+yes_no = [
+    ["yes", "y"],
+    ["no", "n"]
+]
+
+snack_order = []
+
+check_snack = "invalid choice"
+while check_snack == "invalid choice":
+    want_snack = input("Do you want Snacks?").lower()
+    check_snack = string_check(want_snack, yes_no)
+
+if check_snack == "Yes":
+    desired_snack = ""
+
+    while desired_snack != "xxx":
+
+        desired_snack = input("Snack: ").lower()
+
+        if desired_snack == "xxx":
+            break
+
+        snack_choice= ""
+        if re.match(number_regex, desired_snack):
+            amount = int(desired_snack[0])
+            desired_snack = desired_snack[1:]
+
+        else:
+            amount = 1
+            desired_snack = desired_snack
+
+        desired_snack = desired_snack.strip()
+
+        snack_choice = string_check(desired_snack, snack_choice)
+
+        if amount >= 5:
+            print("Sorry - we have a four snack limit!")
+            snack_choice = "inavlid choice"
+
+        amount_snack = "{} {}".format(amount, snack_choice)
+
+        if snack_choice != "xxx" and snack_choice != "invalid choice":
+            snack_order.append(snack_choice)
+
+print()
+if len(snack_order) == 0:
+    print("Snacks Ordered: None")
+
+else:
+    print("Snacks Ordered: ")
+
+    for item in snack_order:
+        print(item)
+
