ad613f073cbf6eaa67f31809957947f310edbe27
magno.franc <magno.franc@awatapu.school.nz>
2022-05-25 13:26:13 +1200

    ugh

---
+++ venv/08_snack_lists_v1.py
@@ -0,0 +1,17 @@
+# initialise snack lists
+
+popcorn = []
+mms = []
+pita_chips = []
+orange_juice = []
+water = []
+
+snack_lists = [popcorn, mms, pita_chips, orange_juice, water]
+
+snack_menu_dict = {
+    'Popcorn': popcorn,
+    'M&Ms': mms,
+    'Pita Chips': pita_chips,
+    'Orange Juice': orange_juice,
+    'Water': water,
+}
